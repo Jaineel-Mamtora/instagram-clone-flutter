fdacf23c092bd46d69179d8e7afe193ad45f9e1e
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2024-01-21 20:36:22 +0530

    added: loade

---
+++ lib/common_widgets/loader.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/core/globals.dart';
+
+import 'package:instagram_clone/my_theme.dart';
+
+class LoaderWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Center(
+        child: CircularProgressIndicator(
+          color: lightTheme.colorScheme.primary,
+          strokeWidth: 3,
+        ),
+      );
+}
+
+class LoaderManager {
+  LoaderManager._sharedInstance();
+  static final LoaderManager _shared = LoaderManager._sharedInstance();
+  factory LoaderManager() => _shared;
+  late OverlayEntry _overlayEntry;
+
+  void showLoader(BuildContext context) {
+    _overlayEntry = OverlayEntry(
+      builder: (BuildContext context) => Container(
+        width: deviceWidth,
+        height: deviceHeight,
+        color: Colors.white60,
+        child: LoaderWidget(),
+      ),
+    );
+
+    Overlay.of(context).insert(_overlayEntry);
+  }
+
+  void hideLoader() {
+    _overlayEntry.remove();
+  }
+}
